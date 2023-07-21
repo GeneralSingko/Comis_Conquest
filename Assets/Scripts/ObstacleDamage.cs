291832e999cd63e88cdfc7517fef7b165a71dc89
John Carlo Pascual <74855892+GeneralSingko@users.noreply.github.com>
2023-07-21 10:12:36 +0800

    fixing the health system a little bit

--- Assets/Scripts/ObstacleDamage.cs
+++ Assets/Scripts/ObstacleDamage.cs
@@ -4,24 +4,22 @@ using UnityEngine;
 
 public class ObstacleDamage : MonoBehaviour
 {
-    public PlayerHealth Healthhealth;
+    private Health Healthhealth;
     public int damage = 2;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player") {
-            Healthhealth.TakeDamage(damage);
+            Healthhealth = collision.gameObject.GetComponent<Health>();
+
+            if (Healthhealth != null)
+            {
+                Healthhealth.numOfHearts -= damage;
+
+                Healthhealth.Hearts();
+            }
         }
+
+        Debug.Log("Collided with Player");
     }
 }
